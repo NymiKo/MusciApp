1b9a16a242455f754db50079fb8873405c57f34a
NymiKo <dimon.kabernik8@gmail.com>
2024-05-02 00:13:23 +0500

    MediaPlayer has been replaced by ExoPlayer

--- composeApp/src/androidMain/kotlin/audio_player/AndroidAudioPlayerController.kt
+++ composeApp/src/androidMain/kotlin/audio_player/AndroidAudioPlayerController.kt
@@ -1,54 +1,71 @@
 package audio_player
 
+import android.content.Context
 import android.media.AudioAttributes
 import android.media.MediaPlayer
+import androidx.annotation.OptIn
+import androidx.compose.runtime.LaunchedEffect
+import androidx.media3.common.MediaItem
+import androidx.media3.common.PlaybackException
+import androidx.media3.common.Player
+import androidx.media3.common.Timeline
+import androidx.media3.common.util.UnstableApi
+import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.exoplayer.trackselection.DefaultTrackSelector
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 
-class AndroidAudioPlayerController : AudioPlayerController {
+class AndroidAudioPlayerController(context: Context) : AudioPlayerController {
 
-    private var mediaPlayer: MediaPlayer? = null
+    private var mediaPlayer = ExoPlayer.Builder(context).build()
+    private val _currentTime = MutableStateFlow<Float>(0F)
+    val currentTime: StateFlow<Float> get() = _currentTime
 
-    private fun initMediaPlayer() {
-        mediaPlayer = MediaPlayer().apply {
-            setAudioAttributes(
-                AudioAttributes.Builder()
-                    .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
-                    .setUsage(AudioAttributes.USAGE_MEDIA)
-                    .build()
-            )
-        }
-    }
-
+    @OptIn(UnstableApi::class)
     override fun prepare(url: String, listener: AudioPlayerListener) {
-        if (mediaPlayer == null) {
-            initMediaPlayer()
-        }
+        val mediaItem = MediaItem.fromUri(url)
+        mediaPlayer.addListener(object : Player.Listener {
+            override fun onPlaybackStateChanged(playbackState: Int) {
+                super.onPlaybackStateChanged(playbackState)
+                when(playbackState) {
+                    Player.STATE_READY -> listener.onReady()
+                    Player.STATE_ENDED -> listener.onAudioFinished()
+                }
+            }
 
-        mediaPlayer?.setDataSource(url)
-        mediaPlayer?.prepareAsync()
+            override fun onPlayerError(error: PlaybackException) {
+                super.onPlayerError(error)
+                listener.onError()
+            }
+        })
+
+        mediaPlayer.setMediaItem(mediaItem)
+        mediaPlayer.prepare()
     }
 
     override fun start() {
-        mediaPlayer?.start()
+        mediaPlayer.play()
     }
 
     override fun pause() {
-        mediaPlayer?.pause()
+        mediaPlayer.pause()
     }
 
     override fun stop() {
-        mediaPlayer?.stop()
+        mediaPlayer.stop()
     }
 
     override fun isPlaying(): Boolean {
-        return mediaPlayer?.isPlaying ?: false
+        return mediaPlayer.isPlaying
     }
 
     override fun getFullTime(): Long {
-        return mediaPlayer?.duration?.toLong() ?: 0L
+        return if (mediaPlayer.playbackState == Player.STATE_READY) {
+            mediaPlayer.duration
+        } else 0L
     }
 
     override fun release() {
-        mediaPlayer?.release()
-        mediaPlayer = null
+        mediaPlayer.release()
     }
 }
--- composeApp/src/androidMain/kotlin/data/di/CommonModuleKoin.android.kt
+++ composeApp/src/androidMain/kotlin/data/di/CommonModuleKoin.android.kt
@@ -10,6 +10,6 @@ import org.koin.dsl.module
 
 actual val commonModule: Module = module {
     single<SongsRepository> { SongsRepositoryImpl() }
-    single<AudioPlayerController> { AndroidAudioPlayerController() }
+    single<AudioPlayerController> { AndroidAudioPlayerController(get()) }
     factory { SongsViewModel(get(), get()) }
 }
