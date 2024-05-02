49382319554d7921f3e2644e7efbcc83f9b46a5a
NymiKo <dimon.kabernik8@gmail.com>
2024-05-02 22:19:33 +0500

    Added serialization for the Song data class

--- composeApp/src/commonMain/kotlin/data/SongsApi.kt
+++ composeApp/src/commonMain/kotlin/data/SongsApi.kt
@@ -1,4 +1,15 @@
 package data
 
-interface SongsApi {
+import data.model.Song
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.request.get
+
+class SongsApi {
+    private val client = HttpClient()
+
+    suspend fun getSongsList(): List<Song> {
+        val response = client.get("http://f0862137.xsph.ru/musicApp/")
+        return response.body()
+    }
 }
