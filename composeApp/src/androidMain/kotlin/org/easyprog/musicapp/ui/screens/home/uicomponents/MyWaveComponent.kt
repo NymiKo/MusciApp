c63849b9b622dd4087a7f62df565fb5df83cba5d
NymiKo <dimon.kabernik8@gmail.com>
2024-05-28 20:37:50 +0500

    Added stopping and playing songs when clicking on my wave

--- composeApp/src/androidMain/kotlin/org/easyprog/musicapp/ui/screens/home/uicomponents/MyWaveComponent.kt
+++ composeApp/src/androidMain/kotlin/org/easyprog/musicapp/ui/screens/home/uicomponents/MyWaveComponent.kt
@@ -2,6 +2,7 @@ package org.easyprog.musicapp.ui.screens.home.uicomponents
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -15,6 +16,7 @@ import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -23,11 +25,19 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import audio_player.AudioPlayerState
 import org.easyprog.musicapp.ui.theme.PurpleDark
 import org.easyprog.musicapp.ui.theme.PurpleLight
 
 @Composable
-fun MyWaveComponent(modifier: Modifier = Modifier, myWaveMode: Boolean, getSongsListMyWave: () -> Unit) {
+fun MyWaveComponent(
+    modifier: Modifier = Modifier,
+    myWaveMode: Boolean,
+    playerState: AudioPlayerState,
+    playSong: () -> Unit,
+    pauseSong: () -> Unit,
+    getSongsListMyWave: () -> Unit
+) {
     Box(
         modifier = Modifier.padding(16.dp).fillMaxWidth().height(200.dp)
             .clip(RoundedCornerShape(24.dp))
@@ -35,15 +45,22 @@ fun MyWaveComponent(modifier: Modifier = Modifier, myWaveMode: Boolean, getSongs
         contentAlignment = Alignment.Center
     ) {
         Row(
-            modifier = Modifier.clickable {
-                getSongsListMyWave()
+            modifier = Modifier.clickable(
+                interactionSource = remember { MutableInteractionSource() },
+                indication = null
+            ) {
+                if (myWaveMode) {
+                    togglePlay(playerState = playerState, playSong = playSong::invoke, pauseSong = pauseSong::invoke)
+                } else {
+                    getSongsListMyWave()
+                }
             },
             verticalAlignment = Alignment.CenterVertically
         ) {
 
             Icon(
-                modifier = Modifier.size(50.dp),
-                imageVector = if (myWaveMode) Icons.Default.Pause else Icons.Default.PlayArrow,
+                modifier = Modifier.size(40.dp),
+                imageVector = if (myWaveMode && playerState == AudioPlayerState.PLAYING) Icons.Default.Pause else Icons.Default.PlayArrow,
                 contentDescription = null,
                 tint = Color.White
             )
@@ -55,5 +72,16 @@ fun MyWaveComponent(modifier: Modifier = Modifier, myWaveMode: Boolean, getSongs
                 fontWeight = FontWeight.ExtraBold
             )
         }
+    }
+}
+
+private fun togglePlay(
+    playerState: AudioPlayerState,
+    playSong: () -> Unit,
+    pauseSong: () -> Unit,
+) {
+    when (playerState) {
+        AudioPlayerState.PLAYING -> pauseSong()
+        else -> playSong()
     }
 }
